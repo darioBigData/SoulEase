0dd0cc47b352753bb2e2d5bdd6c0fedecd53a307
daarioo7 <dariopanadero7@gmail.com>
2025-10-02 00:16:02 +0200

    Resolver conflicto en firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
 // Import the functions you need from the SDKs you need
 import 'firebase/firestore';
 import { initializeApp } from 'firebase/app';
-import { getFirestore} from 'firebase/firestore';
+import { getFirestore } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,25 +17,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-=======
-// Import the functions you need from the SDKs you need
-import 'firebase/firestore';
-import { initializeApp } from 'firebase/app';
-import { getFirestore} from 'firebase/firestore';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyC4MfwMryN59Nc0MNyiK05fOwveYFtO-SU",
-  authDomain: "soulease-prueba.firebaseapp.com",
-  projectId: "soulease-prueba",
-  storageBucket: "soulease-prueba.appspot.com",
-  messagingSenderId: "178779495706",
-  appId: "1:178779495706:web:c49cf10b0082116dd9e0a3"
-};
-
-const app = initializeApp(firebaseConfig);
-
->>>>>>> 63f1369469845096380d206d0ecff5f59b7fc1a1
 export default app;
